4d478cd391b117f39fc7235551f6dcaea9339e5b
Sumit Kumar <sumitnaveen029@gmail.com>
2021-08-12 21:02:45 +0530

    initial commit

---
+++ line.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+a = [1, 2, 3, 4]    #Values
+b = [2, 4, 6, 8]    #Doubles
+
+plt.title("A Line chart")   #Title of chart
+plt.xlabel("Values")    #X-azis name
+plt.ylabel("Double")    #Y-axis name
+
+plt.plot(a, b, 'r', linestyle = 'dashed', marker = '.', label = 'A simple line')
+plt.legend(loc = 'lower right')  #Location of labels
+plt.show()
